3c6b879821f650c31c54a587bcd5d16f1c55f031
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-05 20:02:31 +0500

    05file

--- file05.py
+++ file05.py
@@ -6,5 +6,18 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    f = open(data,'r')
+    data = f.read()
+    t = 0
+    digit_soni = 0
+    alpha_soni = 0
+    while len(data)>t:
+        if data[t].isdigit():
+            digit_soni+=1
+        else:
+            alpha_soni+=1
+        t+=1
+    sonlar = [digit_soni,alpha_soni]
+    return sonlar
+print(main('data/data05.txt'))
 # Read data from file
