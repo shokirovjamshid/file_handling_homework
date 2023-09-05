2b03547ccaf8dc2219bb6acc5fc99c41dacd01b3
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-05 20:04:16 +0500

    09file

--- file09.py
+++ file09.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    f = open(data,mode='r+')
+    data = f.read()
+    for i in data:
+        if i.isdigit():
+            min = int(i)
+    min1 = min
+    for n in data:
+        if n.isdigit():
+            if min1>int(n):
+                min1 = int(n)
+    return min1
+print(main('data/data08.txt'))
 # Read data from file
