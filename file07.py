e2b671494483c7845cf7fa91a479a55e06a7e035
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-05 20:03:30 +0500

    07file

--- file07.py
+++ file07.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    f = open(data, mode='r+')
+    data_1 = f.read()
+    s = 0
+    for i in data_1:
+        if i.isdigit():
+            s+=int(i)
+    return s
+print(main('data/data07.txt')) 
 # Read data from file
