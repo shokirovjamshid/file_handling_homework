5f0ebd461e07d4ea8b9eaa4fa52f5f2b076451e0
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-05 20:01:01 +0500

    02file

--- file02.py
+++ file02.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    f = open(data,'r')
+    data = f.read()
+    t = 0
+    for i in data:
+        t+=1
+    return t
+print(main('data/data02.txt')) 
 # Read data from file
